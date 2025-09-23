f74dddf981a2a19fd4cf20703dbf8feb02b1454e
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-09-23 12:10:14 +0300

    footer middle

--- src/components/footer/FooterMiddle.jsx
+++ src/components/footer/FooterMiddle.jsx
@@ -1,8 +1,25 @@
-import  from 'react'
+import { footerMiddleList } from "../../constants"
 
 function FooterMiddle() {
   return (
-    <div>FooterMiddle</div>
+    <div className="w-full bg-amazon_light text-white">
+        <div className="w-full py-10 border-b-[1px] border-gray-500 ">
+            <div className="max-w-5xl mx-auto text-gray-300 ">
+                <div className="grid grid-cols-4 place-items-center items-start  ">
+                    {footerMiddleList.map((item, index)=> (
+                    <div key={index}>
+                        <h3 className="font-titleFont text-white text-base font-semibold mb-3">{item.title}</h3>
+                        {item.items.map((items,i)=> (
+                            <ul key={i} className="flex flex-col gap-2 font-bodyFont">
+                                <li className="footerLink">{items}</li>
+                            </ul>
+                            ))}
+                    </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -27,3 +27,15 @@ export   const allItems = [
     {_id: "126" , title: "Video Games"},
     {_id: "127 " , title: "Women's Fashion"},
   ]
+
+
+  export const footerMiddleList = [
+    {title:"Get to Know Us" , 
+      items:["Careers", "Blog", "About Amazon", "Investor Relations", "Amazon Devices", "Amazon Science"] },
+    {title:"Make Money with Us" , 
+      items:["Sell products on Amazon", "Sell on Amazon Business", "Sell apps on Amazon", "Become an Affiliate", " Advertise Your Products", "Self-Publish with Us", "Host an Amazon Hub", "›See More Make Money with Us"] },
+    {title:"Amazon Payment Products" , 
+      items:["Amazon Business Card", "Shop with Points", "Reload Your Balance", "Amazon Currency Converter"] },
+    {title:"Let Us Help You" , 
+      items:["Amazon and COVID-19", "Your Account", "Your Orders", "Shipping Rates & Policies", "Returns & Replacements", "Manage Your Content and Devices", "Help"] }
+  ]
