21917861e0653819c6ebebd69a1f8dcb6b575dd7
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-09-25 11:21:24 +0300

    finsih registration page

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -7,45 +7,109 @@ import { useState } from "react";
 
 function Registration() {
 
+  const [clientName, setClientName]= useState("")
   const [email, seEmail]= useState("")
   const [password, setPassword]= useState("")
   const [rePassword, setRePassword]= useState("")
 
+  const [errorClientName, setErrorClientName]= useState("")
+  const [errorEmail, setErrorEmail]= useState("")
+  const [errorPassword, setErrorPassword]= useState("")
+  const [errorRePassword, setErrorRePassword]= useState("")
+
+  function emailValidation(email){
+    return String(email).toLowerCase().match(/^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/)
+  }
+
+  function handleRegistration(e){
+    e.preventDefault()
+    if (!clientName){
+      setErrorClientName("Enter your name")
+    }
+    if (!email){
+      setErrorEmail("Enter your email")
+    }else if(!emailValidation(email)){
+      setErrorEmail("Enter a valid email")
+    }
+    if (!password){
+      setErrorPassword("Enter your password")
+    }else if(password.length < 6){
+      setErrorPassword("Password mush be at least 6 characters")
+    }
+    if (!rePassword){
+      setErrorRePassword("Confirm your password")
+    }else if(rePassword !== password){
+      setErrorRePassword("Password not matches")
+    }
+
+    if(clientName && email && emailValidation(email) && password && password.length >= 6 && rePassword && rePassword === password){
+      setClientName("")
+      seEmail("")
+      setPassword("")
+      setRePassword("")
+    }
+
+
+  }
+
   return (
     <div className="w-full">
       <div className="bg-gray-100 pb-10 ">
-        <form className="flex items-center flex-col w-[370px] mx-auto ">
+        <form onSubmit={handleRegistration} className="flex items-center flex-col w-[370px] mx-auto ">
           <img src={darkLogo} alt="logo" className="w-32" />
           <div className="border-zinc-200 p-6 border w-full">
             <h2 className="text-3xl font-titleFont font-medium mb-4">Create Acount </h2>
             <div className="flex flex-col gap-3">
               <div className="flex flex-col gap-2">
                 <p className="text-sm font-medium">Your name</p>
-                <input type="text" className="w-full lowercase py-1 border border-zinc-400 px-2 text-balance rounded-sm outline-none focus:border-[#e77600] focus:shadow-amazonInput duration-100 "    />
+                <input type="text" className="w-full lowercase py-1 border border-zinc-400 px-2 text-balance rounded-sm outline-none focus:border-[#e77600] focus:shadow-amazonInput duration-100 " 
+                value={clientName}
+                onChange={(e)=>{ setClientName(e.target.value); setErrorClientName("")}}
+                />
+                {errorClientName && (
+                  <p className="text-red-600 text-xs font-semibold tracking-wide flex items-center gap-2 -mt-1.5">
+                    <span className="italic font-extrabold font-titleFont text-base">!</span>{errorClientName}
+                  </p>
+                )}
               </div>
               <div className="flex flex-col gap-2">
                 <p className="text-sm font-medium">Email or mobile phone number</p>
                 <input type="email"
-                value={email}
-                onChange={(e)=> seEmail(e.target.value)}
-                className="w-full lowercase py-1 border border-zinc-400 px-2 text-balance rounded-sm outline-none focus:border-[#e77600] focus:shadow-amazonInput duration-100 "    />
+                value={email} required
+                onChange={(e)=>{ seEmail(e.target.value) ; setErrorEmail("")}}
+                className="w-full lowercase py-1 border border-zinc-400 px-2 text-balance rounded-sm outline-none focus:border-[#e77600] focus:shadow-amazonInput duration-100 "/>
+                {errorEmail && (
+                  <p className="text-red-600 text-xs font-semibold tracking-wide flex items-center gap-2 -mt-1.5">
+                    <span className="italic font-extrabold font-titleFont text-base">!</span>{errorEmail}
+                  </p>
+                )}
               </div>
               <div className="flex flex-col gap-2">
                 <p className="text-sm font-medium">Password</p>
                 <input type="password"
                 value={password}
-                onChange={(e)=> setPassword(e.target.value)}
-                className="w-full lowercase py-1 border border-zinc-400 px-2 text-balance rounded-sm outline-none focus:border-[#e77600] focus:shadow-amazonInput duration-100 "    />
+                onChange={(e)=> {setPassword(e.target.value); setErrorPassword("")}}
+                className="w-full py-1 border border-zinc-400 px-2 text-balance rounded-sm outline-none focus:border-[#e77600] focus:shadow-amazonInput duration-100 "    />
+                {errorPassword && (
+                  <p className="text-red-600 text-xs font-semibold tracking-wide flex items-center gap-2 -mt-1.5">
+                    <span className="italic font-extrabold font-titleFont text-base">!</span>{errorPassword}
+                  </p>
+                )}
               </div>
               <div className="flex flex-col gap-2">
                 <p className="text-sm font-medium">Re-enter Password</p>
                 <input type="password"
                 value={rePassword}
-                onChange={(e)=> setRePassword(e.target.value)}
-                className="w-full lowercase py-1 border border-zinc-400 px-2 text-balance rounded-sm outline-none focus:border-[#e77600] focus:shadow-amazonInput duration-100 "    />
+                onChange={(e)=> {setRePassword(e.target.value); setErrorRePassword("")}}
+                className="w-full  py-1 border border-zinc-400 px-2 text-balance rounded-sm outline-none focus:border-[#e77600] focus:shadow-amazonInput duration-100 "    />
+                {errorRePassword && (
+                  <p className="text-red-600 text-xs font-semibold tracking-wide flex items-center gap-2 -mt-1.5">
+                    <span className="italic font-extrabold font-titleFont text-base">!</span>{errorRePassword}
+                  </p>
+                )}
                 <p className="text-xs text-gray-600">Passwords must be at least 6 characters.</p>
               </div>
-              <button type="button" className="w-full text-sm py-1.5 font-normal rounded-sm bg-gradient-to-t from-[#f7dfa5] to-[#f0c14b] hover:bg-gradient-to-b border border-zinc-400 active:border-yellow-800 active:shadow-amazonInput ">Continue</button>
+              <button  className="w-full text-sm py-1.5 font-normal rounded-sm bg-gradient-to-t from-[#f7dfa5] to-[#f0c14b] hover:bg-gradient-to-b border border-zinc-400 active:border-yellow-800 active:shadow-amazonInput ">Continue</button>
             </div>
             <p className="text-xs text-black leading-4 mt-4">By creating an account, you agree to Amazon's <span className="text-blue-600 cursor-pointer">Conditions of Use  </span > and <span className="text-blue-600 cursor-pointer">Privacy Notice.</span>
             </p>
